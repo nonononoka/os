41692aacda5ee19ad5828206aed1b0a1ec1a0bbd
nonononoka <onon.kaori@gmail.com>
2025-02-10 11:15:03 +0900

    カーネルパニック

--- kernel.c
+++ kernel.c
@@ -40,10 +40,10 @@ void putchar(char ch){
 void kernel_main(void) {
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    for(;;){
-        __asm__ __volatile__("wfi");
-    }
+    PANIC("booted!");
+    printf("unreachable here!\n");
 }
 
 // この関数を.text.boot セクションに配置する
